0042a09c043e9b51681cf8fd2fd7819b31438f23
miya264 <tmiyajima117@gmail.com>
2025-02-13 15:50:55 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'standalone',
   env: {
-    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   }
 }
 
 module.exports = nextConfig
+
